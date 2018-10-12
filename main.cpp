d2296e1cd9d1122f3d29aff2249c9d4e9a227bb5
Jacob <36747872+jacobcwright@users.noreply.github.com>
2018-10-11 20:14:49 -0600

    finished product

--- main.cpp
+++ main.cpp
@@ -6,17 +6,30 @@ using namespace std;
 int main() {
 	string userString;
 	bool palindrome = 0;
+	int lastIndex;
 
 
 	cout << "A palidrome is a word that is the same forward as it is backwards..." << endl << endl;
 	cout << "Enter a word to check if it is a palidrome: " << endl;
 	cin >> userString;
+	lastIndex = userString.size();
+
+	for (unsigned int i = 0; i < userString.size(); i++) {
+		if (userString.at(i) == userString.at(lastIndex - i - 1)) {
+			palindrome = true;
+		}
+		else {
+			palindrome = false;
+			break;
+		}
+	}
+
 
 	if (palindrome) {
-		cout << userString << " is a palidrome!" << endl;
+		cout << endl << userString << " is a palidrome!" << endl;
 	}
 	else if (!palindrome) {
-		cout << userString << " is not a palindrome!" << endl;
+		cout << endl << userString << " is not a palindrome!" << endl;
 	}
 
 
